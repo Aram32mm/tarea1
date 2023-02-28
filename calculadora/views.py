b689139b1d257b1e34627eedb17cae1a8dee311c
Aram32mm <a01657142@tec.mx>
2023-02-28 12:46:18 -0600

    avance 1

--- calculadora/views.py
+++ calculadora/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here 
+def nueva():
+    return 0
+
+def index(request):
+    #return HttpResponse("Bienvenido")
+    return render(request,'index.html')
+
+def procesamiento(request):
+    nombre = request.POST['nombre']
+    nombre = nombre.title()
+    return render(request, 'proceso.html',{'name':nombre})
+    
